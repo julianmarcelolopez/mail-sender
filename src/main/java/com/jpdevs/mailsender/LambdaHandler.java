194576a1cfbcb489d02a97dc8112166a86cb2446
JulianLopez <julianmarcelolopez@gmail.com>
2025-03-02 18:27:32 -0300

    fixing aws libs version

--- src/main/java/com/jpdevs/mailsender/LambdaHandler.java
+++ src/main/java/com/jpdevs/mailsender/LambdaHandler.java
@@ -1,14 +1,15 @@
 package com.jpdevs.mailsender;
 
-import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
-import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.model.SingleValueHeaders;
-import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,46 +19,37 @@ public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent
 
 	static {
 		try {
-			// Inicializa el handler correctamente para aplicaciones servlet
 			handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(MailSenderApplication.class);
 		} catch (ContainerInitializationException e) {
-			e.printStackTrace();
-			throw new RuntimeException("No se pudo inicializar la aplicación Spring Boot", e);
+			throw new RuntimeException("Could not initialize Spring Boot application", e);
 		}
 	}
 
 	@Override
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-		// Redirige la raíz a Swagger UI
-		if ("/".equals(input.getPath()) || "/dev".equals(input.getPath())) {
+		// Redirect root path to Swagger UI
+		if (input.getPath().equals("/") || input.getPath().equals("/dev")) {
 			Map<String, String> headers = new HashMap<>();
-			headers.put("Location", "/swagger-ui/index.html");
+			headers.put("Location", "/swagger-ui.html");
 
 			return new APIGatewayProxyResponseEvent()
 					.withStatusCode(302)
 					.withHeaders(headers);
 		}
 
-		// Convierte la solicitud API Gateway en AwsProxyRequest
+		// Use the handler for all other requests
 		AwsProxyRequest awsProxyRequest = new AwsProxyRequest();
-		awsProxyRequest.setPath(input.getPath());
 		awsProxyRequest.setHttpMethod(input.getHttpMethod());
 		awsProxyRequest.setBody(input.getBody());
-		SingleValueHeaders singleValueHeaders = new SingleValueHeaders();
-		if (input.getHeaders() != null) {
-			input.getHeaders().forEach(singleValueHeaders::put);
-		}
-		awsProxyRequest.setHeaders(singleValueHeaders);
+		awsProxyRequest.setHeaders((SingleValueHeaders) input.getHeaders());
 		awsProxyRequest.setQueryStringParameters(input.getQueryStringParameters());
 		awsProxyRequest.setPathParameters(input.getPathParameters());
 
-		// Procesa la solicitud con Spring Boot
 		AwsProxyResponse response = handler.proxy(awsProxyRequest, context);
 
 		return new APIGatewayProxyResponseEvent()
 				.withStatusCode(response.getStatusCode())
 				.withHeaders(response.getHeaders())
-				.withBody(response.getBody())
-				.withIsBase64Encoded(response.isBase64Encoded());
+				.withBody(response.getBody());
 	}
 }
